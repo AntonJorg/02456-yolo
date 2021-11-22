d98b84280bd19cc7a14822bd6b51ec8264a2896b
AntonJorg <anton2410@gmail.com>
2021-11-22 13:29:27 +0100

    Dataloader outputs targets as list of tensors

--- model/dataloader.py
+++ model/dataloader.py
@@ -9,6 +9,46 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
 
+class_dict = {
+    "DHelmet": 0,
+    "DNoHelmet": 1,
+    "DHelmetP0Helmet": 2,
+    "DHelmetP0NoHelmet": 3,
+    "DNoHelmetP0NoHelmet": 4,
+    "DHelmetP1Helmet": 5,
+    "DNoHelmetP1Helmet": 6,
+    "DHelmetP1NoHelmet": 7,
+    "DNoHelmetP1NoHelmet": 8,
+    "DHelmetP0HelmetP1Helmet": 9,
+    "DHelmetP0NoHelmetP1Helmet": 10,
+    "DHelmetP0NoHelmetP1NoHelmet": 11,
+    "DNoHelmetP0NoHelmetP1Helmet": 12,
+    "DNoHelmetP0NoHelmetP1NoHelmet": 13,
+    "DNoHelmetP0HelmetP1NoHelmet": 14,
+    "DHelmetP1HelmetP2Helmet": 15,
+    "DHelmetP1NoHelmetP2Helmet": 16,
+    "DHelmetP1HelmetP2NoHelmet": 17,
+    "DHelmetP1NoHelmetP2NoHelmet": 18,
+    "DNoHelmetP1HelmetP2Helmet": 19,
+    "DNoHelmetP1NoHelmetP2Helmet": 20,
+    "DNoHelmetP1NoHelmetP2NoHelmet": 21,
+    "DHelmetP0HelmetP1HelmetP2Helmet": 22,
+    "DHelmetP0HelmetP1NoHelmetP2Helmet": 23,
+    "DHelmetP0HelmetP1NoHelmetP2NoHelmet": 24,
+    "DHelmetP0NoHelmetP1HelmetP2Helmet": 25,
+    "DHelmetP0NoHelmetP1NoHelmetP2Helmet": 26,
+    "DHelmetP0NoHelmetP1NoHelmetP2NoHelmet": 27,
+    "DNoHelmetP0NoHelmetP1NoHelmetP2NoHelmet": 28,
+    "DNoHelmetP0NoHelmetP1NoHelmetP2Helmet": 29,
+    "DHelmetP1NoHelmetP2NoHelmetP3NoHelmet": 30,
+    "DHelmetP1NoHelmetP2NoHelmetP3Helmet": 31,
+    "DNoHelmetP1NoHelmetP2NoHelmetP3NoHelmet": 32,
+    "DNoHelmetP0NoHelmetP1NoHelmetP2NoHelmetP3NoHelmet": 33,
+    "DHelmetP0NoHelmetP1NoHelmetP2NoHelmetP3Helmet": 34,
+    "DHelmetP0NoHelmetP1NoHelmetP2NoHelmetP3NoHelmet": 35
+}
+
+
 class HELMETDataSet(Dataset):
     def __init__(self, root_dir, resize=True):
         # working directories
@@ -54,15 +94,24 @@ class HELMETDataSet(Dataset):
         x = Image.open(self.images_paths[idx])
 
         # target
-        y = self.images_annotations[idx]
+        target = self.images_annotations[idx]
 
-        if self.resize:
-            y[:, 2:6] *= np.array([832/1920, 832/1080, 832/1920, 832/1080])
-            y[:, 2:6] = np.round(y[:, 2:6].astype(float))
+        d = {i: dict_from_bounding_box(row) for i, row in enumerate(target)}
 
-        y = {i: row for i, row in enumerate(y)}
+        y = []
+        for k, v in d.items():
+            y.append(torch.tensor(np.append(class_dict[v["class"]], v["dim"])).unsqueeze(0))
 
-        return self.transform(x), y
+        if len(y) != 0:
+            y = torch.cat(y, 0)
+        else:
+            y = torch.tensor(y)
+
+        if self.resize and len(y) != 0:
+            y *= np.array([1, 832/1920, 832/1080, 832/1920, 832/1080])
+            y = np.round(y)
+
+        return self.transform(x), y, d
 
 
 class HELMETDataLoader(DataLoader):
@@ -73,14 +122,16 @@ class HELMETDataLoader(DataLoader):
     @staticmethod
     def custom_collate_fn(batch):
         # change such that images are one tensor and not tuple of tensors
-        imgs, annotations = tuple(zip(*batch))
+        imgs, targets, annotations = tuple(zip(*batch))
         imgs = [im.unsqueeze(0) for im in imgs]
         imgs = torch.cat(imgs, 0)
-        return imgs, annotations
+        targets = [t for t in targets]
+        #targets = torch.cat(targets, 0)
+        return imgs, targets, annotations
 
 
 def dict_from_bounding_box(bb):
-    d = {"track_id": bb[0], "frame_id": bb[1], "dim": bb[2:6], "class": bb[6]}
+    d = {"track_id": bb[0], "frame_id": bb[1], "dim": bb[2:6].astype(int), "class": bb[6]}
     return d
 
 
@@ -101,13 +152,15 @@ def pos_encoding_from_label(label):
 
 
 if __name__ == "__main__":
-    dataloader = HELMETDataLoader("../data/HELMET_DATASET")
+    dataloader = HELMETDataLoader("../data/HELMET_DATASET", resize=True)
 
     batch = next(iter(dataloader))
 
-    imgs, annotations = batch
+    imgs, targets, annotations = batch
 
     print(imgs.shape)
+    print(len(targets))
+    print(targets)
     print(annotations)
     print(dataloader.dataset.images_annotations[:5])
 
