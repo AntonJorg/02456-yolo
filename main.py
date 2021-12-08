f5acbd3c761de2d4558f90afa815993d77ee0c2d
AntonJorg <anton2410@gmail.com>
2021-12-08 09:31:49 +0100

    New example.gif

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from torch.utils.data import DataLoader
-from model.dataloader import HELMETDataSet, HELMETDataLoader
+from model.dataloader import HELMETDataSet, HELMETDataLoader, class_dict
 
-dataloader = HELMETDataLoader("data/HELMET_DATASET", batch_size=12)
+report_examples = ["Mandalay_1_118/93.jpg",
+                   "Mandalay_2_57/40.jpg",
+                   "NyaungU_rural_32/23.jpg",
+                   "Pathein_rural_2/86.jpg"]
 
-batch = next(iter(dataloader))
+dataset = HELMETDataSet(root_dir="data/HELMET_DATASET", split="test")
 
-imgs, annotations = batch
+idx = []
 
-print(imgs)
-print(annotations)
+for i, p in enumerate(dataset.images_paths):
+    for ex in report_examples:
+        if ex in p:
+            print(i, p)
+            idx.append(i)
 
-fig, axs = plt.subplots(3, 4)
+cmap = plt.cm.get_cmap("hsv", len(class_dict))
 
-for (x, y), ax in np.ndenumerate(axs):
-    img, annotation = imgs[4*x + y], annotations[4*x + y]
+for i in idx:
+    img, targets, annotation = dataset[i]
+
+    # Figure
+    fig = plt.figure(figsize=(19.20, 10.80), dpi=100)
+    ax = fig.gca()
+
+    ax.set_axis_off()
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.margins(0, 0)
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+
     ax.imshow(np.array(img).transpose([1, 2, 0]))
+    ax.axis('off')
 
-    for k, v in annotation.items():
-        _, _, x, y, w, h, _ = v
-        rect = plt.Rectangle((x, y), w, h, fc="none", ec="red")
+    # Draw bounding boxes and labels of detections
+
+    for _, a in annotation.items():
+        x, y, w, h = a["dim"]
+
+        label = a["class"]
+        col = cmap(class_dict[label])
+        rect = plt.Rectangle((x, y), w, h, fc="none", ec=col, linewidth=8)
         ax.add_patch(rect)
+        ax.text(x, y, label, fontsize=15, bbox={'facecolor': col, 'pad': 2})
 
-plt.show()
+    plt.savefig(f"output/report_ex_{i}.jpg", dpi=100)
+    plt.close()
